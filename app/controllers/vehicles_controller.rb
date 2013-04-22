d824fec038406b9b2c0579834bc85208d44c7ecb
Harry Curotta <hcurotta@gmail.com>
2013-04-22 19:59:42 +1000

    uncomment

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -8,21 +8,21 @@ class VehiclesController < ApplicationController
   
     respond_to do |format|
       format.html {
-        # agent = Mechanize.new
-        #       locations = Vehicle.get_pods(agent)
-        #       
-        #       @vehicles.each do |vehicle|
-        #         locations.each do |location|
-        #           if vehicle.pod_id == location[:pod_id]
-        #             if location[:available] == nil
-        #               vehicle.destroy
-        #             else
-        #               vehicle.available = location[:available]
-        #               vehicle.save
-        #             end
-        #           end
-        #         end
-        #       end
+      agent = Mechanize.new
+                  locations = Vehicle.get_pods(agent)
+                  
+                  @vehicles.each do |vehicle|
+                    locations.each do |location|
+                      if vehicle.pod_id == location[:pod_id]
+                        if location[:available] == nil
+                          vehicle.destroy
+                        else
+                          vehicle.available = location[:available]
+                          vehicle.save
+                        end
+                      end
+                    end
+                  end
       }
       format.json { render json: @vehicles }
     end
